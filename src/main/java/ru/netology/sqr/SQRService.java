af5cc869814b9856a2238153acaba74b67fe58b7
Kuchumova Vera <weryn4ik86@gmail.com>
2022-05-25 13:32:18 +0300

    new reformat code

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,15 +2,13 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int amountSqr (int min, int max) {
-
-        int amount =0;
-        for (int i=10; i <=99; i++){
-            if (i*i >= min &&  i*i <=max) {
-                amount ++;
+    public int amountSqr(int min, int max) {
+        int amount = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= min && i * i <= max) {
+                amount++;
             }
         }
-        return  amount;
+        return amount;
     }
-
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -3,42 +3,37 @@ package ru.netology.sqr;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class SQRServiceTest {
 
     @Test
-    public void ShouldAmountSqr() {
+    public void shouldAmountSqr() {
         SQRService service = new SQRService();
         int actual = service.amountSqr(200, 300);
         int expected = 3;
-
         Assertions.assertEquals(actual, expected);
     }
 
     @Test
-    public void ShouldAmountSqr1() {
+    public void shouldAmountSqrMinEqualMax() {
         SQRService service = new SQRService();
         int actual = service.amountSqr(200, 200);
         int expected = 0;
-
         Assertions.assertEquals(actual, expected);
     }
 
     @Test
-    public void ShouldAmountSqr2() {
+    public void shouldAmountSqrMinZero() {
         SQRService service = new SQRService();
         int actual = service.amountSqr(0, 300);
         int expected = 8;
-
         Assertions.assertEquals(actual, expected);
     }
 
     @Test
-    public void ShouldAmountSqr3() {
+    public void shouldAmountSqrMinMoreMax() {
         SQRService service = new SQRService();
         int actual = service.amountSqr(300, 0);
         int expected = 0;
-
         Assertions.assertEquals(actual, expected);
     }
 }
